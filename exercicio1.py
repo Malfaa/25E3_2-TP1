e24b114326c92c042aa525ce94a593c26b786ae4
marufa <gmalfacorrea@gmail.com>
2025-07-29 22:48:17 -0300

    Primeiro commit, exercícios 1 ao 3

---
+++ exercicio1.py
@@ -0,0 +1,8 @@
+
+nome_estagiario = "Gustavo"
+idade_estagiario = 25
+codigo_departamento = "25E3_2"
+
+print(nome_estagiario)
+print(idade_estagiario)
+print(codigo_departamento)
---
+++ exercicio2.py
@@ -0,0 +1,9 @@
+nome_cliente = 'Gustavo'
+idade_cliente = 25
+score_credito = 772.4
+cliente_ativo = True
+
+print("Nome:", nome_cliente, " | tipo do dado: " + str(type(nome_cliente)))
+print("Idade: ", idade_cliente, " | tipo do dado: " + str(type(idade_cliente)))
+print("Score: ", score_credito, " | tipo do dado: " + str(type(score_credito)))
+print("Status: ", cliente_ativo, " | tipo do dado: " + str(type(cliente_ativo)))
